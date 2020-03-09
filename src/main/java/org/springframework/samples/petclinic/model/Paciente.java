31172e7f6a3b152f9490e844036e470d20a436bd
alvalfric <alvalfric@users.noreply.github.com>
2020-03-09 20:06:08 +0100

    HU-003

---
+++ src/main/java/org/springframework/samples/petclinic/model/Paciente.java
@@ -0,0 +1,47 @@
+
+package org.springframework.samples.petclinic.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Paciente extends NamedEntity {
+
+	@NotBlank
+	private String		nombre;
+
+	private String		apellidos;
+
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate	f_nacimiento;
+
+	@Length(min = 9, max = 9)
+	private String		DNI;
+
+	private Integer		n_telefono;
+
+	private String		domicilio;
+
+	@Email
+	private String		email;
+
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate	f_alta;
+
+	@ManyToOne
+	@JoinColumn(name = "medico_id")
+	@NotNull
+	private Medico		medico;
+}
