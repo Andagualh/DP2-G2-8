ede56bc9a0d0c511be53eba4907f51b1f65d7f38
Iván Cárdenas <ivacarmen@alum.us.es>
2020-03-10 21:59:38 +0100

    Añadido Entidad HistoriaClinica

---
+++ src/main/java/org/springframework/samples/petclinic/model/Medico.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.petclinic.model;
+
+import javax.persistence.Entity;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Medico extends NamedEntity {
+
+	private String	nombre;
+
+	private String	apellidos;
+
+	@Length(min = 9, max = 9)
+	private String	DNI;
+
+	@Length(min = 9, max = 9)
+	private String	n_telefono;
+
+	private String	domicilio;
+
+	private boolean	activo;
+}
