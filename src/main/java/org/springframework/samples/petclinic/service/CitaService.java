c8ae2163ffbe9cdffc613c769568def13920e659
Iván Cárdenas <ivacarmen@alum.us.es>
2020-03-18 10:07:44 +0100

    hu 007 parte 1
    añadido controlador y servicio pero aun no probado

--- src/main/java/org/springframework/samples/petclinic/service/CitaService.java
+++ src/main/java/org/springframework/samples/petclinic/service/CitaService.java
@@ -1,14 +1,50 @@
 
 package org.springframework.samples.petclinic.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Cita;
 import org.springframework.samples.petclinic.repository.CitaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CitaService {
 
 	@Autowired
-	private CitaRepository citaRepository;
+	private CitaRepository citaRepo;
+	
+	@Transactional
+	public int citaCount() {
+		return (int) citaRepo.count();
+	}
+	
+	@Transactional
+	public Iterable<Cita> findAll(){
+		return citaRepo.findAll();
+	}
+	
+	//No esta terminada
+	@Transactional
+	public Iterable<Cita> findAllByMedicoId(int medicoId){
+		return citaRepo.findAll();
+	}
+	
+	@Transactional
+	public void save(Cita cita){
+		citaRepo.save(cita);
+	}
+	
+	@Transactional
+	public void delete(Cita cita){
+		citaRepo.delete(cita);
+	}
+	
+	@Transactional
+	public Optional<Cita> findCitaById(int citaId){
+		return citaRepo.findById(citaId);
+	}
+	
 
 }
