5cf4314a9369de2a3fc953992184d6d4686a17e9
alvalfric <56022145+alvalfric@users.noreply.github.com>
2020-03-27 19:11:33 +0100

    Fixed cita

--- src/main/java/org/springframework/samples/petclinic/model/Cita.java
+++ src/main/java/org/springframework/samples/petclinic/model/Cita.java
@@ -3,9 +3,7 @@ package org.springframework.samples.petclinic.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
@@ -24,7 +22,7 @@ public class Cita extends NamedEntity {
 
 	private String		lugar;
 
-	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@ManyToOne//(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JoinColumn(name = "paciente_id")
 	@NotNull
 	private Paciente	paciente;
