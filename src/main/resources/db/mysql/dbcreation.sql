f35daa81ebdb01c1f480ded33849d1fc988cebeb
Andagualh <56022166+Andagualh@users.noreply.github.com>
2020-05-04 20:29:08 +0200

    Update dbcreation.sql

--- src/main/resources/db/mysql/dbcreation.sql
+++ src/main/resources/db/mysql/dbcreation.sql
@@ -1,6 +1,10 @@
 drop database if exists `medical`;
 create database `medical`;
 
+drop user if exists 'medical'@'localhost';
+CREATE USER 'medical'@'localhost' IDENTIFIED BY 'medical';
+
+
 grant select, insert, update, delete, create, drop, references, index, alter, 
         create temporary tables, lock tables, create view, create routine, 
         alter routine, execute, trigger, show view
