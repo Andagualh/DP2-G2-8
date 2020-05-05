8c1e416346a5d286c8705791661dc77c7aa8b49a
Andagualh <56022166+Andagualh@users.noreply.github.com>
2020-05-05 21:39:51 +0200

    Update InformeServiceTest.java

--- src/test/java/org/springframework/samples/petclinic/service/InformeServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/InformeServiceTest.java
@@ -189,7 +189,7 @@ public class InformeServiceTest {
         es casi el mismo caso que con las counts que 
         se cambian día si y día también.
         Además el método está probado en un test anterior*/
-        assertEquals(7, informeService.findInformeById(informe.getId()).get().getId());
+        assertEquals(informe.getId(), informeService.findInformeById(informe.getId()).get().getId());
     }
 
     @Test
