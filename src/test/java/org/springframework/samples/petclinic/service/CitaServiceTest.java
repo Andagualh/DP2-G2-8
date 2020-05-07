1258178f4faf8d9ac317d414741265ec2e6ebd67
Andagualh <56022166+Andagualh@users.noreply.github.com>
2020-05-07 17:28:24 +0200

    tests

--- src/test/java/org/springframework/samples/petclinic/service/CitaServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/CitaServiceTest.java
@@ -24,7 +24,7 @@ import org.springframework.samples.petclinic.model.User;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
-@AutoConfigureTestDatabase(replace=Replace.NONE)
+
 public class CitaServiceTest {
 
 	@Autowired
