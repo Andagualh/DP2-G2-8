d578f80530d43c139222f9a7f5538725a417d862
PedroCoza <56022203+PedroCoza@users.noreply.github.com>
2020-05-11 17:22:14 +0200

    Minor fixes for coherence added

--- src/test/java/org/springframework/samples/petclinic/service/CitaServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/CitaServiceTest.java
@@ -146,10 +146,10 @@ public class CitaServiceTest {
 	@Test
 	public void testCountWithInitialData() {
 		int count = this.citaService.citaCount();
-		Assertions.assertEquals(count, 9);
+		Assertions.assertEquals(count, 11);
 	}
 	
-	int citaInitalCountData = 9;
+	int citaInitalCountData = 11;
 
 	@Test
 	public void testCreateCita() throws InvalidAttributeValueException {
@@ -181,7 +181,7 @@ public class CitaServiceTest {
 		
 		this.citaService.delete(this.citaService.findCitaById(3).get());
 
-		Assertions.assertEquals(this.citaService.citaCount(),8);
+		Assertions.assertEquals(this.citaService.citaCount(),10);
 
 	}
 	
