4e9f164032b18022b1875ab010195b5c0142c537
ivanhr3 <ivaherrod1@alum.us.es>
2020-05-24 20:03:38 +0200

    Update TratamientoServiceTest.java

--- src/test/java/org/springframework/samples/petclinic/service/TratamientoServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/TratamientoServiceTest.java
@@ -1,6 +1,9 @@
 package org.springframework.samples.petclinic.service;
 
 import java.time.LocalDate;
+import java.util.Optional;
+
+import javax.management.InvalidAttributeValueException;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
@@ -8,7 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Authorities;
+import org.springframework.samples.petclinic.model.Cita;
+import org.springframework.samples.petclinic.model.HistoriaClinica;
+import org.springframework.samples.petclinic.model.Informe;
+import org.springframework.samples.petclinic.model.Medico;
+import org.springframework.samples.petclinic.model.Paciente;
 import org.springframework.samples.petclinic.model.Tratamiento;
+import org.springframework.samples.petclinic.model.User;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -18,6 +29,14 @@ public class TratamientoServiceTest {
 	private TratamientoService		tratamientoService;
 	@Autowired 
 	private InformeService   		informeService;
+	@Autowired
+	private CitaService citaService;
+	@Autowired
+    private PacienteService pacienteService;
+	@Autowired
+    private HistoriaClinicaService historiaClinicaService;
+	@Autowired
+	private MedicoService medicoService;
 	
 	@Test
 	public void testCountWithInitialData() {
@@ -186,6 +205,100 @@ public class TratamientoServiceTest {
 			this.tratamientoService.save(tratamiento);
 		});
 		*/
+	}
+	
+	public Cita createDummyCita1(final Paciente paciente) throws InvalidAttributeValueException {
+        Cita cita = new Cita();
+        cita.setPaciente(paciente);
+        cita.setFecha(LocalDate.now());
+        cita.setLugar("Consulta 1");
+        citaService.save(cita);
+        return cita;
+    }
+	
+	public Paciente createDummyPaciente(final Medico medico, final HistoriaClinica hs) {
+        Paciente paciente = new Paciente();
+        paciente.setNombre("Paciente 1");
+        paciente.setApellidos("Apellidos");
+        paciente.setF_nacimiento(LocalDate.of(1996, 01, 12));
+        paciente.setDNI("12345678A");
+        paciente.setDomicilio("Sevilla");
+        paciente.setEmail("paciente@email.com");
+        paciente.setF_alta(LocalDate.now());
+        paciente.setMedico(medico);
+
+        this.pacienteService.pacienteCreate(paciente);
+
+        hs.setDescripcion("placeholder");
+        hs.setPaciente(paciente);
+        this.historiaClinicaService.saveHistoriaClinica(hs);
+
+        return paciente;
+    }
+
+	
+	public Informe createDummyInforme(final Cita cita) throws DataAccessException, IllegalAccessException {
+		Informe informe = new Informe();
+        informe.setCita(cita);
+        informe.setDiagnostico("Dermatitis");
+        informe.setMotivo_consulta("Picor en frente");
+        informeService.saveInforme(informe);
+        return informe;
+	}
+	
+	public Medico createDummyMedico() {
+        Medico medico = new Medico();
+        User medicoUser = new User();
+        Authorities authorities = new Authorities();
+
+        medico.setNombre("Medico 1");
+        medico.setApellidos("Apellidos");
+        medico.setDNI("12345678A");
+        medico.setN_telefono("123456789");
+        medico.setDomicilio("Domicilio");
+        medicoUser.setUsername("medico1");
+        medicoUser.setPassword("medico1");
+        medicoUser.setEnabled(true);
+        medico.setUser(medicoUser);
+        authorities.setUsername(medicoUser.getUsername());
+        authorities.setAuthority("medico");
+
+        this.medicoService.medicoCreate(medico);
+
+        return medico;
+    }
+	
+	@Test
+    public void testDeleteTratamientoSuccess() throws DataAccessException, IllegalAccessException, InvalidAttributeValueException{
+		
+		Tratamiento tratamiento = new Tratamiento();
+		Medico medico = createDummyMedico();
+		Paciente paciente = createDummyPaciente(medico, new HistoriaClinica());
+		Cita cita = createDummyCita1(paciente);
+		citaService.save(cita);
+		Informe informe = new Informe();
+		informe.setCita(cita);
+		informe.setDiagnostico("Dermatitis");
+        informe.setMotivo_consulta("Picor en frente");
+        informeService.saveInforme(informe);
+        //cita.setInforme(informe);
+		
+		tratamiento.setMedicamento("aspirina1");
+		tratamiento.setDosis("1 pastilla cada 8 horas");
+		tratamiento.setF_inicio_tratamiento(LocalDate.parse("2020-06-23"));
+		tratamiento.setF_fin_tratamiento(LocalDate.parse("2020-10-25"));
+		tratamiento.setInforme(informe);
+		
+		LocalDate fecha = tratamiento.getInforme().getCita().getFecha();
+		int idMedico = tratamiento.getInforme().getCita().getPaciente().getMedico().getId();
+		
+		Assertions.assertNotNull(tratamiento);
+		Assertions.assertEquals(LocalDate.now(), fecha);
+		
+		this.tratamientoService.save(tratamiento);
+		tratamientoService.deleteTratamiento(tratamiento.getId(), idMedico);
+		
+		Assertions.assertEquals(Optional.empty(), tratamientoService.findTratamientoById(tratamiento.getId()));
 	}
 
 }
