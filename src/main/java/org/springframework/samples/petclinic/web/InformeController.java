e8843e3d3b0f34d475164c146bbd522e3ea0c3d2
Andagualh <56022166+Andagualh@users.noreply.github.com>
2020-06-01 19:42:06 +0200

    Update InformeController.java

--- src/main/java/org/springframework/samples/petclinic/web/InformeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/InformeController.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
